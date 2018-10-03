312582c225baf2cfe96fcaa414019bd0b555efbf
singuerinc <nahuel.scotti@gmail.com>
2018-10-03 16:28:10 +0200

    feat: use rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,46 @@
+import { interval } from "rxjs";
+import { map, scan, startWith } from "rxjs/operators";
 import { Random } from "./utils";
 
-function rndColor(rnd: number): string {
-  const rgbColors = 16777215;
-  return (
-    "#" +
-    Math.floor((rnd / 4294967294) * rgbColors)
-      .toString(16)
-      .padEnd(6, "0")
-  );
-}
+const INTERVAL = 1879.69219;
+const toHex = (num: number) =>
+  Math.floor(num)
+    .toString(16)
+    // @ts-ignore
+    .padEnd(6, "0");
 
-function random(seed: number): number {
-  return new Random(seed).next();
-}
+const rndToColor = (rnd: number): string =>
+  "#" + toHex((rnd / 4294967294) * 16777215);
 
-function render(elem1: HTMLDivElement, elem2: HTMLDivElement, color: string) {
+const random = (seed: number): number => new Random(seed).next();
+
+const nextRndFrom = (limit: number, time: number): string[] =>
+  Array(limit)
+    .fill(0)
+    .map((v, i) => time + INTERVAL * i)
+    .map(rndToColor);
+
+const calcSeed = (every, start, time) => Math.floor((time - start) / every);
+
+const render = (
+  elem1: HTMLDivElement,
+  elem2: HTMLDivElement,
+  color: string
+) => {
   elem1.style.color = color;
   elem1.innerText = color;
   elem2.style.backgroundColor = color;
-}
+};
 
-const INTERVAL = 1879.69219;
-const start = new Date(new Date().getFullYear(), 0, 1, 0, 0, 0, 0).getTime();
+const START = new Date(new Date().getFullYear(), 0, 1, 0, 0, 0, 0).getTime();
 const txt = document.querySelector<HTMLDivElement>(".color .txt");
 const bg = document.querySelector<HTMLDivElement>(".colors");
+const source = interval(INTERVAL);
 
-setInterval(() => {
-  const now = new Date().getTime();
-  const seed = Math.floor((now - start) / INTERVAL);
-  const r = random(seed);
-  const color = rndColor(r);
-
-  render(txt, bg, color);
-}, INTERVAL);
-
-// render(txt, bg, "#212325");
+source
+  .pipe(startWith(START))
+  .pipe(scan<number>(() => calcSeed(INTERVAL, START, new Date().getTime())))
+  .pipe(map((x: number) => rndToColor(random(x))))
+  .subscribe(color => {
+    render(txt, bg, color);
+  });
